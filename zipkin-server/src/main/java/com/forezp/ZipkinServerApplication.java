77dd0d5279568dd6025c61e86523b37082a8d3ca
congyang <congyang@rayclear.com>
2018-04-03 20:02:45 +0800

    sleuth-stream

--- zipkin-server/src/main/java/com/forezp/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/forezp/ZipkinServerApplication.java
@@ -3,11 +3,11 @@ package com.forezp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import zipkin.server.EnableZipkinServer;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableZipkinServer
+@EnableZipkinStreamServer
 public class ZipkinServerApplication {
 
     public static void main(String[] args) {
